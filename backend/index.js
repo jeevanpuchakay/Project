00204309f6806750f2382a26f5facdbef4df515e
jeevan <jeevangkr37@gmail.com>
2019-11-05 16:13:27 +0530

    Addition of routes to history,timeLine,block

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,15 @@
 const express = require('express');
 
-const mysql= require('mysql');
+
 
 const app=express();
 
 const port =5000;
 
 
-var bodyParser = require('body-parser')
-
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
+var bodyParser = require('body-parser'); 
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(port,()=>{
     console.log(`listening on port ${port}`);
@@ -27,5 +26,18 @@ const cats=require('./routes/vehiclesAndFares');
 app.use('/cat',cats);
 
 
+const timeLine=require('./routes/timeLine')
+
+app.use('/timeLine',timeLine);
+
+
+const history=require('./routes/history')
+
+app.use('/history',history);
+
+
 const connection=require('./db/index');
+
+//app.use(express.bodyParser());
+  //app.use(app.router);
 
