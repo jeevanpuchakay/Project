ea5168c50a90a23e4600361b2f9cc37ebe5c6d4f
jeevan <jeevangkr37@gmail.com>
2019-11-06 13:35:23 +0530

    Complete Home Page layout

--- client/src/components/navigation/homeNavBar.js
+++ client/src/components/navigation/homeNavBar.js
@@ -1,41 +1,72 @@
-import React, { Component } from 'react'
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import AppBar from '@material-ui/core/AppBar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import data from './../data/drawer.json'
+import {Link} from 'react-router-dom'
 
-//navBar from bootstrap
 
-import { Navbar } from 'react-bootstrap'
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+  },
+  drawer: {
+    width: '10rem',
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: '10rem',
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+  toolbar: theme.mixins.toolbar,
+}));
 
+export default function ClippedDrawer() {
+  const classes = useStyles();
 
-//@material-ui
-
-//1.Drawer 
-import { Drawer } from '@material-ui/core'
-//2.List
-import { List, ListItem, ListItemText } from '@material-ui/core'
-
-
-export default class HomeNavBar extends Component {
-    render() {
-        return (
-            <div>
-                <Navbar bg="dark" variant="dark" style={{ zIndex: 1, height: '4em' }}>
-                    <Navbar.Brand href="/">
-                        Admin
-                    </Navbar.Brand>
-                </Navbar>
-                <Drawer open={1} style={{ zIndex: -2, width: '4em' }}>
-                    <List style={{ marginTop: '4em' }}>
-                        {
-                            ["DashBoard", "Users", "Category", "Add vehicle"].map((item, index) => (
-                                (<ListItem style={{fontSize:'1em'}}>
-                                    <ListItemText>
-                                        {item}
-                                    </ListItemText>
-                                </ListItem>)
-                            ))
-                        }
-                    </List>
-                </Drawer>
-            </div>
-        )
-    }
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar position="fixed" className={classes.appBar}>
+        <Toolbar>
+          <Typography variant="h6" noWrap>
+            Admin
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        className={classes.drawer}
+        variant="permanent"
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <div className={classes.toolbar} />
+        <List style={{alignItems:'center'}}>
+          {data.map((text, index) => (
+            <ListItem button component={Link} to={"/"+text.A} key={text}>
+              <ListItemText primary={text.A} />
+            </ListItem>
+          ))}
+        </List>
+       
+      </Drawer>
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        
+      </main>
+    </div>
+  );
 }
