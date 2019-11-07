1c17d30231e7fc87a6aa5d3f29ba644d8eb78d17
jeevan <jeevangkr37@gmail.com>
2019-11-07 13:34:40 +0530

    Addition add user while exit

--- backend/routes/history.js
+++ backend/routes/history.js
@@ -13,18 +13,22 @@ router.route('/').post(async(req,res)=>{
 
     let  q=`insert into history set ?`;
     let a;
-    console.log(req.body)
     try{
         //hpending
         //have to add intime while removing outtime
-        a={userId:`${req.body.userId}`,vehicleId:`${req.body.vehicleId}`,
-        userName:`${req.body.userName}`,vehicleType:req.body.vehicleType
+        a={userId:`${req.body.userId}`,
+        vehicleId:`${req.body.vehicleId}`,
+        vehicleType:req.body.vehicleType,
+       // inTime:req.body.inTime
+        
     };
     }
     catch(e){
         return e;
     }
-    connection.query(q,a,async(err,rows,fields)=>{
+    //userName:`${req.body.userName}`,
+    console.log(a)
+    connection.query(q,await a,async(err,rows,fields)=>{
         if(err)
         res.status(400).json("Error: "+err);
         res.json(await rows);
